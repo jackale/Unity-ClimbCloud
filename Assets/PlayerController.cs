24b280801f394be5ab561beef665b6cde8c48581
jackale <melon119@gmail.com>
2019-06-06 23:33:33 +0900

    Chapter6-6

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 780.0f;
+    float walkFroce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // 左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // 無限に加速しないように上限値で留める
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+        if (speedx < maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkFroce);
+        }
+
+        // 動く方向に応じて反転（追加）
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+    }
+}
