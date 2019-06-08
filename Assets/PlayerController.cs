ff7e6c64949e243433ff7c0917469a71a38aa33f
jackale <melon119@gmail.com>
2019-06-09 02:43:56 +0900

    Work with mobile

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float walkFroce = 30.0f;
     float maxWalkSpeed = 2.0f;
     Animator animator;
+    float threshold = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,16 +23,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // ジャンプ
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+        if (Input.GetMouseButtonDown(0)
+            && Mathf.Approximately(this.rigid2D.velocity.y, 0f))
+#else
         if (Input.GetKeyDown(KeyCode.Space)
-            && Mathf.Approximately(this.rigid2D.velocity.y, 0f)) 
+          && Mathf.Approximately(this.rigid2D.velocity.y, 0f))
+#endif
         {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
         // 左右移動
         int key = 0;
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < -this.threshold) key = -1;
+#else
         if (Input.GetKey(KeyCode.RightArrow)) key = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+#endif
 
         // 無限に加速しないように上限値で留める
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
@@ -52,7 +64,15 @@ public class PlayerController : MonoBehaviour
         }
 
         // プレイヤの速度に応じてアニメーション速度を変える
-        this.animator.speed = speedx / 2.0f;
+        if (Mathf.Approximately(this.rigid2D.velocity.y, 0f))
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
